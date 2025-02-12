59387c75ccecfa417df2855282835465f66ce40a
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-12 01:13:40 +0000

    added the multiply.py file to week02 directory

---
+++ week02/multiply.py
@@ -0,0 +1,7 @@
+# multiply.py
+# This program reads in two numbers and multiplies them
+# Author: Stephen Kerr
+
+answer = 111 * 555
+# The answer wasn't printed as there was no print function. See added below 
+print(answer)
