b825e986b1385b8eb63a47e64fdda9ebf593a354
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-16 22:29:44 +0000

    added testType.py for week 3's lab

---
+++ week03_lab/testTypes.py
@@ -0,0 +1,29 @@
+# Create 5 variables of different types and print them to the console
+# int, float, boolean, str, list, use the type() function to check the type of the variables.
+# Author: Stephen Kerr
+
+
+# variable 1 is type class = 'int' and value = 3
+i = 3
+print(f'The value of i = {i}')
+print(f'The class type of i = {type(i)}\n')
+
+# variable 2 is type class = 'float' and value = 3.5
+fl = 3.5
+print(f'The value of ifl = {fl}')
+print(f'The class type of fl = {type(fl)}\n')
+
+# variable 3 is type class = 'boolean' and value = True
+is_ = True
+print(f'The value of is = {is_}')
+print(f'The class type of is = {type(is_)}\n')
+
+# variable 4 is type class = 'str' and value =  'how now Brown Cow'
+memo = "how now Brown Cow"
+print(f'The value of memo = {memo}')
+print(f'The class type of memo = {type(memo)}\n')
+
+# variable 5 is type class = 'list' and value =  '[1, 2, 3, 4, 5]'
+lots = [1, 2, 3, 4, 5]
+print(f'The value of lots = {lots}')
+print(f'The class type of lots = {type(lots)}\n')
