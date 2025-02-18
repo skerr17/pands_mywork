2de75dbf637ce16495160ee68f28a2a69c283a30
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-18 22:47:51 +0000

    did the first exercise of lab3.3

---
+++ week03_lab/lab3.3.1_lenght.py
@@ -0,0 +1,11 @@
+# this program reads a string an outputs the length of the string
+# Author: Stephen Kerr
+
+# Prompt the user for a string 
+input_string = str(input("Enter a string: "))
+
+# calculate the lenght of the string
+string_lenght = len(input_string)
+
+# print the lenght of the string out
+print(f"The lenght of the string \"{input_string}\" is {string_lenght}")
