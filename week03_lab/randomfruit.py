edaa5d22744020196efdfd05c79801b62f8b603d
Stephen Kerr <kerrstephen17@gmail.com>
2025-02-18 21:43:43 +0000

    finished the week 3 lab first worksheet Q6, 7 and 8 are in the randomfruit.py file

--- week03_lab/randomfruit.py
+++ week03_lab/randomfruit.py
@@ -1,4 +1,6 @@
 # this program prints out a random fruit
+# Also answers Q6, Q7, Q8 in the week 3 lab
+# Author: Stephen Kerr
 
 # import the random module
 import random
@@ -10,3 +12,22 @@ fruits = ['Apple', 'Orange', 'Banana', 'Pinapple', 'strawberry']
 index = random.randint(0, len(fruits)-1)
 
 print(f'A Random Fruit: {fruits[index]}')
+
+# Q6 Why does this code cause an error? How can we fix it? 
+'''
+message = 'I have eaten ' + 99 + ' burritos.'
+print (message)
+'''
+# This code causes an error because you cannot concatenate a string with an integer.
+# To fix this error, the integer 99 needs to be converted to a string.
+# The code should be changed to:
+message = 'I have eaten ' + str(99) + ' burritos.'
+print (message)
+
+# Q7 Why is eggs a valid variable name while 100 is invalid? 
+# Varriable names cannot start with a number, they can start with a letter or an underscore
+# 100 is invalid because it starts with a number this will cause a SyntaxError
+# eggs is valid because it starts with a letter
+
+# What three functions can be used to get the integer, floating-point number, or string version of a value?
+# int(), float(), str()
